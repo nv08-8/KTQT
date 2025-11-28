716173866d7de556abcda7b706c053a40103f277
HaVu-lala <dieulien2005@gmail.com>
2025-11-28 14:47:18 +0700

    UI category

--- app/src/main/java/vn/hcmute/ktqt/adapters/CategoryAdapter.java
+++ app/src/main/java/vn/hcmute/ktqt/adapters/CategoryAdapter.java
@@ -3,10 +3,13 @@ package vn.hcmute.ktqt.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +46,11 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.VH> {
     public void onBindViewHolder(@NonNull VH holder, int position) {
         Category c = items.get(position);
         holder.name.setText(c.name);
+        // You can set a placeholder image or use Glide to load image from a URL if your Category model has an image field
+        // For now, I'll just use a placeholder
+        Glide.with(holder.itemView.getContext())
+                .load(R.drawable.ic_launcher_background) // Replace with your actual image logic
+                .into(holder.image);
         holder.itemView.setOnClickListener(v -> listener.onItemClick(c));
     }
 
@@ -52,10 +60,12 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.VH> {
     }
 
     static class VH extends RecyclerView.ViewHolder {
+        ImageView image;
         TextView name;
 
         public VH(@NonNull View itemView) {
             super(itemView);
+            image = itemView.findViewById(R.id.ivCategory);
             name = itemView.findViewById(R.id.tvCategoryName);
         }
     }
